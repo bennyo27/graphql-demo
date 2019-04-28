4fb986e5889c4a8930ce18d2786075aa3a6565df
bennyo27 <oseguerabenny27@gmail.com>
2019-04-27 18:44:33 -0700

    Added: Added reviews data and queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,24 +1,49 @@
 // import graphql
 const graphql = require("graphql");
+// import lodash to use array?
+const _ = require("lodash");
 
 // import graphql types
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
+} = graphql;
 
-// dummy data
+// dummy data users
 var users = [
     { name: 'User One', id: '1' },
     { name: 'User Two', id: '2' },
     { name: 'User Three', id: '3' },
 ];
 
+// dummy data reviews
+var reviews = [
+    {name: "Review One", review: 5, id: "1"},
+    {name: "Review Two", review: 1, id: "2"},
+    {name: "Review Three", review: 4, id: "3"},
+]
+
 // create type of object User
 const UserType= new GraphQLObjectType({
     name: "User",
     fields: () => ({
-        id: {type: GraphQLString },
+        id: {type: GraphQLID },
         name: {type: GraphQLString }
     })
 });
+
+// create type of object review
+const ReviewType = new GraphQLObjectType({
+    name: "Review",
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        review: {type: GraphQLInt}
+    })
+})
 
 // create root query
 const RootQuery = new GraphQLObjectType({
@@ -26,10 +51,18 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         user: {
             type: UserType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args) {
                 // code to get data from db / other source
-                return __dirname.find(users, {id: args.id})
+                return _.find(users, {id: args.id})
+            }
+        },
+        review: {
+            type: ReviewType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                // code to get data from db / other sources
+                return _.find(reviews, {id: args.id})
             }
         }
     }
